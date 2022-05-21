e07a9880186ecc691cec365218235f63c4f56b92
Olivia Lind <eo.lind@gmail.com>
2022-05-21 16:13:33 -0500

    completed exercise

--- main.js
+++ main.js
@@ -11,8 +11,23 @@ const names = [
     "The Great Wall of China",
 ]
 
-console.log("All Pace Names")
+console.log("All Place Names: ", names.join(", "))
+console.log(" ");
+console.log("----------------");
+console.log(" ")
+
+// for (const name of names) {
+//     if (name.startsWith("The ")) {
+//         console.log(name)
+//     }
+// }
+
+const namesWithArticlesArr = []
 
 for (const name of names) {
-    console.log(name)
+    if (name.startsWith("The ")) {
+        namesWithArticlesArr.push(name)
+    }
 }
+
+console.log("Names beginning with an article: ", namesWithArticlesArr.join(", "));
